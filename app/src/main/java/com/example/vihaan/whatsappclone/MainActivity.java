d8b8ccbaad0a29c40039d8389affa8a148fc9458
Vihaan <vihaanverma21@gmail.com>
2017-05-20 19:47:02 +0530

    initviews defined

--- app/src/main/java/com/example/vihaan/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/vihaan/whatsappclone/MainActivity.java
@@ -5,9 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews()
+    {
+        initToolbar();
+        initNewMessageFloatingButton();
+    }
+
+    private void initToolbar()
+    {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
+    private void initNewMessageFloatingButton()
+    {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
