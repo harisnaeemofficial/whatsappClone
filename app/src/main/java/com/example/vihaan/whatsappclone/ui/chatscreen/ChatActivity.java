c4b0408a27e5c264aca668a2e796d9489d9ae682
Vihaan <vihaanverma21@gmail.com>
2017-05-22 00:55:34 +0530

    added action icons

--- app/src/main/java/com/example/vihaan/whatsappclone/ui/chatscreen/ChatActivity.java
+++ app/src/main/java/com/example/vihaan/whatsappclone/ui/chatscreen/ChatActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
@@ -70,4 +71,16 @@ public class ChatActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chat, menu);
+        return true;
+    }
 }
