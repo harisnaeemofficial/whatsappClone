a7b9bf18252e57a9fc83523a9a2ea7f3bcc43b3a
Vihaan <vihaanverma21@gmail.com>
2017-06-16 01:47:31 +0530

    creat user screen template added

--- app/src/main/java/com/example/vihaan/whatsappclone/ui/createUserScreen/CreateUserActivity.java
+++ app/src/main/java/com/example/vihaan/whatsappclone/ui/createUserScreen/CreateUserActivity.java
@@ -1,4 +1,4 @@
-package com.example.vihaan.whatsappclone.ui.verifyPhoneScreen;
+package com.example.vihaan.whatsappclone.ui.createUserScreen;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,29 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.vihaan.whatsappclone.R;
-import com.example.vihaan.whatsappclone.ui.createUserScreen.CreateUserActivity;
+import com.example.vihaan.whatsappclone.ui.homescreen.MainActivity;
 
 /**
  * Created by vihaan on 16/06/17.
  */
 
-public class VerifyPhoneActivity extends AppCompatActivity implements View.OnClickListener {
-
+public class CreateUserActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verify_phone);
+        setContentView(R.layout.activity_create_user);
         findViewById(R.id.nextBtn).setOnClickListener(this);
     }
 
+
     @Override
     public void onClick(View v) {
-
-        Intent intent = new Intent(this, CreateUserActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
     }
 }
-
-
