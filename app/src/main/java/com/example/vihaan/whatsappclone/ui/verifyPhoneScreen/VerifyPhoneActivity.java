1bcf81d7b4f7e55e5b15cbeaec2fe8044034db77
Vihaan <vihaanverma21@gmail.com>
2017-06-16 01:14:03 +0530

    verify phone screen

---
+++ app/src/main/java/com/example/vihaan/whatsappclone/ui/verifyPhoneScreen/VerifyPhoneActivity.java
@@ -0,0 +1,23 @@
+package com.example.vihaan.whatsappclone.ui.verifyPhoneScreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.vihaan.whatsappclone.R;
+
+/**
+ * Created by vihaan on 16/06/17.
+ */
+
+public class VerifyPhoneActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone);
+    }
+}
+
+
--- app/src/main/java/com/example/vihaan/whatsappclone/ui/welcomeScreen/WelcomeActivity.java
+++ app/src/main/java/com/example/vihaan/whatsappclone/ui/welcomeScreen/WelcomeActivity.java
@@ -1,20 +1,37 @@
 package com.example.vihaan.whatsappclone.ui.welcomeScreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.vihaan.whatsappclone.R;
+import com.example.vihaan.whatsappclone.ui.verifyPhoneScreen.VerifyPhoneActivity;
 
 /**
  * Created by vihaan on 15/06/17.
  */
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        findViewById(R.id.agreeNContinueTVBtn).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.agreeNContinueTVBtn:
+                Intent intent = new Intent(this, VerifyPhoneActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
     }
 }
