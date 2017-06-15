34a16901d729290e47f16c9e2f68d56e3e523058
Vihaan <vihaanverma21@gmail.com>
2017-06-16 00:46:40 +0530

    welcome activity added

---
+++ app/src/main/java/com/example/vihaan/whatsappclone/ui/welcomeScreen/WelcomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.vihaan.whatsappclone.ui.welcomeScreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.vihaan.whatsappclone.R;
+
+/**
+ * Created by vihaan on 15/06/17.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
