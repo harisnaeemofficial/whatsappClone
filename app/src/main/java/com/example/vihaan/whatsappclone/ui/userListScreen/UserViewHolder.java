6dc7f5a590e0584c9a76cd45deb9c94a01692095
Vihaan <vihaanverma21@gmail.com>
2017-06-18 19:45:41 +0530

    showing user list

--- app/src/main/java/com/example/vihaan/whatsappclone/ui/userListScreen/UserViewHolder.java
+++ app/src/main/java/com/example/vihaan/whatsappclone/ui/userListScreen/UserViewHolder.java
@@ -1,12 +1,14 @@
 package com.example.vihaan.whatsappclone.ui.userListScreen;
 
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.vihaan.whatsappclone.R;
 import com.example.vihaan.whatsappclone.ui.models.User;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by vihaan on 18/06/17.
@@ -28,5 +30,11 @@ public class UserViewHolder extends RecyclerView.ViewHolder {
 
     public void bindToUser(User user, View.OnClickListener starClickListener) {
 
+        if(!TextUtils.isEmpty(user.getProfilePicUrl()))
+        {
+            Picasso.with(userIV.getContext()).load(user.getProfilePicUrl()).into(userIV);
+        }
+        usernameTV.setText(user.getName());
+        statusTV.setText(user.getStatus());
     }
 }
