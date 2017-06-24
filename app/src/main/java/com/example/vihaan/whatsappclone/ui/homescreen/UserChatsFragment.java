34890ec58ac541974e033f56406345ea4a3db8f4
Vihaan <vihaanverma21@gmail.com>
2017-06-24 15:14:17 +0530

    home screen user chat linked to chat activity

--- app/src/main/java/com/example/vihaan/whatsappclone/ui/homescreen/UserChatsFragment.java
+++ app/src/main/java/com/example/vihaan/whatsappclone/ui/homescreen/UserChatsFragment.java
@@ -14,13 +14,17 @@ import android.view.ViewGroup;
 import com.example.vihaan.whatsappclone.R;
 import com.example.vihaan.whatsappclone.ui.Database;
 import com.example.vihaan.whatsappclone.ui.chatscreen.ChatActivity;
+import com.example.vihaan.whatsappclone.ui.models.User;
 import com.example.vihaan.whatsappclone.ui.models.UserChat;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by vihaan on 20/05/17.
@@ -95,9 +99,23 @@ public class UserChatsFragment extends Fragment {
                     @Override
                     public void onClick(View v) {
                         // Launch PostDetailActivity
-                        Intent intent = new Intent(getActivity(), ChatActivity.class);
-                        intent.putExtra(ChatActivity.EXTRAS_USER, userChat);
-                        startActivity(intent);
+                        DatabaseReference userNode = FirebaseDatabase.getInstance().getReference().child(Database.NODE_USERS).child(userChat.getUid());
+
+                        userNode.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                User user = dataSnapshot.getValue(User.class);
+                                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                                intent.putExtra(ChatActivity.EXTRAS_USER, user);
+                                startActivity(intent);
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
                     }
                 });
 
